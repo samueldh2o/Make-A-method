87f5acb1a1e88571217e7bd578906a1c6f64be0d
Samuel Hoekwater <sdhoekwater@gmail.com>
2020-09-30 16:35:29 -0400

    Initial Push

---
+++ Make A method/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Transactions;
+
+namespace Make_A_method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            age = DesplayGetUserAge();
+        }
+
+       
+        
+        
+        static int DesplayGetUserAge()
+        {
+            int age;
+            string userResponse;
+            bool validResponce;
+
+            do
+            {
+                validResponce = true;
+
+                Console.Write("Enter age.");
+                userResponse = Console.ReadLine();
+
+                if (!int.TryParse(userResponse, out age))
+                {
+                    Console.WriteLine("Please Enter Valid Age");
+                    validResponce = false;
+                }
+
+            } while (!validResponce);
+
+
+            return age;
+
+           
+           
+            
+            
+        } 
+        
+    }
+}
